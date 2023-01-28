e07ffb1fb7ac0018f81209a61660d0e68696cc7f
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2023-01-28 18:43:09 +0600

    first cd

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,10 +1,79 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h3>This is Header</h3>
-        </div>
+        <header className="header">
+            <nav className="nav container">
+                <a href="index.html" className="nav-logo">Rabby</a>
+                <div className="nav-menu">
+                    <ul className="nav-list grid">
+
+                        {/* 0 */}
+                        <li className="nav-item">
+                            <a href="#home" className="nav-link">
+                                <i className="uil uil-estate nav-icon">
+                                  Home  
+                                </i>
+                            </a>
+                        </li>
+                        {/* 1 */}
+                        <li className="nav-item">
+                            <a href="#about" className="nav-link">
+                                <i className="uil uil-user nav-icon">
+                                    About
+                                </i>
+                            </a>
+                        </li>
+                        {/* 2 */}
+                        <li className="nav-item">
+                            <a href="#skills" className="nav-link">
+                                <i className="uil uil-file-alt nav-icon">
+                                    Skills
+
+                                </i>
+                            </a>
+                        </li>
+                        {/* 3 */}
+                        <li className="nav-item">
+                            <a href="#services" className="nav-link">
+                                <i className="uil uil-briefcase nav-icon">
+                                    services
+
+                                </i>
+                            </a>
+                        </li>
+
+                        {/* 4 */}
+                        <li className="nav-item">
+                            <a href="#portfolio" className="nav-link">
+                                <i className="uil uil-scenery nav-icon">
+                                Portfolio
+                                </i>
+                            </a>
+                        </li>
+
+                        {/* 5 */}
+                        <li className="nav-item">
+                            <a href="#contract" className="nav-link">
+                                <i className="uil uil-message nav-icon">
+                                Contract
+                                </i>
+                            </a>
+                        </li>
+
+                       
+                    </ul>
+
+
+                    <i class="uil uil-times nav-close"></i>
+                </div>
+
+                <div className="nav-toggle">
+                <i class="uil uil-apps"></i>
+                </div>
+            </nav>
+        </header>
     );
 };
 
