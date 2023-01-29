4c2c6ae7b71a2fc158716b7e3244ce43fa970acb
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2023-01-29 18:50:56 +0600

    first cddllkll'

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Data from './Data';
+import './Home.css'
+import Social from './Social';
+
 
 const Home = () => {
     return (
        <section className="home" id='home'>
         <div className="home-container">
             <div className="home-content grid">
-                
+        <Social></Social>
+        <div className="home-img">
+            <Data></Data>
+        </div>
             </div>
         </div>
        </section>
--- src/Component/Home/Social.jsx
+++ src/Component/Home/Social.jsx
@@ -3,9 +3,18 @@ import React from 'react';
 const Social = () => {
     return (
         <div className="home-social">
-            <a href="#po" className="home-social-icon" target="_blank"></a>
-            <a href="#po" className="home-social-icon" target="_blank"></a>
-            <a href="#po" className="home-social-icon" target="_blank"></a>
+            <a href="#po" className="home-social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+            </a>
+            <a href="#po" className="home-social-icon" target="_blank">
+            <i class="uil uil-facebook-f"></i>
+            </a>
+            <a href="#po" className="home-social-icon" target="_blank">
+            <i class="uil uil-linkedin-alt"></i>
+            </a>
+            <a href="#po" className="home-social-icon" target="_blank">
+            <i class="uil uil-github"></i>
+            </a>
             
         </div>
     );
